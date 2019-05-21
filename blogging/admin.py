6d97f17134566128a96e21288367eb6d00d29213
Dan Cornutt <dacornutt@gmail.com>
2019-05-20 20:24:54 -0700

    Attempting the Inline portion and migrations...this might go badly.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.StackedInline):
+    model = Category
+
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ('title', 'text', 'Author')
+    inlines = [
+        CategoryInline,
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('name', 'description')
+
+
+# admin.site.register(Post)
+# admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
